8ea33ae45b3c9eb3d44bcd79d9816c89f099a0d0
Anshul Kapoor <anshulkapoor018@gmail.com>
2020-03-18 21:42:23 -0400

    api live on heroku

--- index.js
+++ index.js
@@ -5,21 +5,6 @@ const configRoutes = require("./routes");
 app.use(express.json());
 configRoutes(app);
 
-app.set('port', (process.env.PORT || 5000));
-// app.listen(PORT, () => {
-//     console.log(`Our app is running on port ${ PORT }`);
-// });
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-
-// app.listen(process.env.PORT, '0.0.0.0')
-//   , () => {
-//   console.log("We've now got a server!");
-//   console.log("Your routes will be running on http://localhost:3000");
-// });
